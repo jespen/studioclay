66f63a4e46bc8f10693581b6d7db5647cdd160e6
petjen <jens.sahlstrom@gmail.com>
2025-03-07 15:15:58 +0100

    Switch to static site export for GoDaddy hosting compatibility

--- next.config.js
+++ next.config.js
@@ -3,11 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com'],
+    unoptimized: true,
   },
   eslint: {
     // Disable ESLint during production builds
     ignoreDuringBuilds: true,
   },
+  // Enable static exports
+  output: 'export',
 };
 
 module.exports = nextConfig; 
