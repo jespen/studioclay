158cabafc9f5d764764a793c48d5fb67177a1c83
petjen <jens.sahlstrom@gmail.com>
2025-03-13 10:37:58 +0100

    Update PageProps interface in CoursePage component to handle asynchronous search parameters, improving parameter resolution for better data handling.

--- src/app/admin/dashboard/courses/[id]/page.tsx
+++ src/app/admin/dashboard/courses/[id]/page.tsx
@@ -4,7 +4,7 @@ interface PageProps {
   params: Promise<{
     id: string;
   }>;
-  searchParams: { [key: string]: string | string[] | undefined };
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }
 
 export default async function CoursePage({ params }: PageProps) {
