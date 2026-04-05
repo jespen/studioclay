98bb6c7646ffa53311c4e0391fb491a2e8582baf
petjen <jens.sahlstrom@gmail.com>
2026-04-05 17:07:30 +0200

    Refactor Services component: Remove a private lesson service entry and adjust CSS grid layout to display two columns instead of three for improved responsiveness.

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -30,19 +30,6 @@ Oavsett om ni planerar en kick-off, en teambuilding eller bara vill göra något
       ),
       link: '/contact',
       audience: ['Individuell', 'Företag']
-    },
-    {
-      id: 3,
-      title: 'Privatlektion i drejning för två – en kreativ stund att dela',
-      description: `Vill du överraska din partner, vän eller dejt med något utöver det vanliga? Boka en exklusiv privatlektion i drejning – en lyxig, kreativ upplevelse bara för er två.
-Under tre timmar får ni tillgång till vår studio och en erfaren keramiker som vägleder er genom varje steg vid drejskivan.`,
-      icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" width="20" height="20">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-        </svg>
-      ),
-      link: '/contact',
-      audience: ['Individuell', 'Företag']
     }
   ];
 
